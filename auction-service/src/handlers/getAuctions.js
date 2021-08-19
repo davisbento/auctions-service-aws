012f81ccec367929bff50d23d1af3038d5083ae0
Davi <davisilva4222@gmail.com>
2021-08-19 00:23:42 -0300

    feat: get auctions function

---
+++ auction-service/src/handlers/getAuctions.js
@@ -0,0 +1,29 @@
+import AWS from 'aws-sdk';
+
+import middy from '@middy/core';
+import httpJsonBodyParser from '@middy/http-json-body-parser';
+import httpEventNormalizer from '@middy/http-event-normalizer';
+import httpErrorHandler from '@middy/http-error-handler';
+import createError from 'http-errors';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function getAuctions(event, context) {
+	try {
+		const result = await dynamodb
+			.scan({
+				TableName: process.env.AUCTIONS_TABLE_NAME
+			})
+			.promise();
+
+		return {
+			statusCode: 200,
+			body: JSON.stringify(result.items)
+		};
+	} catch (err) {
+		console.error(err);
+		throw createError.InternalServerError(err);
+	}
+}
+
+export const handler = middy(getAuctions).use(httpJsonBodyParser()).use(httpEventNormalizer()).use(httpErrorHandler());
