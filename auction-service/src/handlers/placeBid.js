922df1ae9242203c23ca59a265b9b3cd5b79b1c0
Davi <davisilva4222@gmail.com>
2021-08-24 10:11:22 -0300

    feat: validate bid amount

--- auction-service/src/handlers/placeBid.js
+++ auction-service/src/handlers/placeBid.js
@@ -1,12 +1,23 @@
 import AWS from 'aws-sdk';
 import createError from 'http-errors';
 import commonMiddleware from '../lib/commonMiddleware';
+import { getAuctionById } from './getAuction';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function placeBid(event, context) {
 	const { id } = event.pathParameters;
 	const { amount } = event.body;
+
+	const auction = await getAuctionById(id);
+
+	if (!auction) {
+		throw createError.NotFound('Auction not found');
+	}
+
+	if (amount <= auction.highestBid.amount) {
+		throw createError.BadRequest('Bid amount must be higher than the current highest bid');
+	}
 
 	const params = {
 		TableName: process.env.AUCTIONS_TABLE_NAME,
